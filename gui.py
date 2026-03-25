5c9f136d143cdf6cd40ea421f1ef4d88eb0b644c
sudoenterusername <ediz.yigitoglu@bahcesehir.edu.tr>
2026-03-25 14:19:24 +0300

    "Add" button and bigger ui

--- gui.py
+++ gui.py
@@ -2,9 +2,21 @@ from functions import *
 import FreeSimpleGUI as FSG
 
 label = FSG.Text("todo")
-input_box = FSG.InputText(tooltip="Enter todo")
+input_box = FSG.InputText(tooltip="Enter todo", key = "todo")
 add_button = FSG.Button("Add")
 
-window = FSG.Window("Gimu", layout = [[label], [input_box, add_button]])
-window.read()
+window = FSG.Window("Gimu",
+                    layout = [[label], [input_box, add_button]],
+                    font = ('Helvetica', 15))
+while True:
+    event, values = window.read()
+    print(event, values)
+    match event:
+        case "Add":
+            todos = get_todos()
+            todos.append(values["todo"] + "\n")
+            add_button.text = "Add"
+            write_todos(todos)
+        case FSG.WIN_CLOSED:
+            break
 window.close()
