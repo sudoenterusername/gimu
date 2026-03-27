288b26fedd3fc3d87ecee40677aed2bf93d52749
sudoenterusername <ediz.yigitoglu@bahcesehir.edu.tr>
2026-03-27 13:53:44 +0300

    To-do items can now be added to the web app

--- web.py
+++ web.py
@@ -3,10 +3,18 @@ from functions import *
 
 todos = get_todos()
 
+def add_todo():
+    todo = st.session_state["new_todo"]
+    todos.append(todo + "\n")
+    write_todos(todos)
+
 st.title("Gimu")
 st.subheader("Basic to-do web app")
 
 for todo in todos:
     st.checkbox(todo)
 
-st.text_input("Add new to-do", placeholder="Add new to-do", label_visibility="hidden")
+st.text_input("Add new to-do", placeholder="Add new to-do",
+              label_visibility="hidden", on_change=add_todo, key="new_todo")
+
+st.session_state
